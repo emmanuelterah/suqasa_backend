822e53715c703c1140f833666232ed710ee283ea
Emmanuel Terah <emmanuelterah98@gmail.com>
2024-04-18 15:52:44 +0300

    authentication stuff

--- app.py
+++ app.py
@@ -51,37 +51,72 @@ from models.payment import Payment
 from models.property import Property
 from models.tenant import Tenant
 from flask_migrate import Migrate
-from flask import Flask
-from models.routes import routes
-
+from flask import Flask, request, jsonify, session
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from models.user import db, User
 
 app = Flask(__name__)
-app.register_blueprint(routes)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
-Migrate(app, db)
-# Define your routes and other application logic here
+app.config['SECRET_KEY'] = 'micasa-suqasa'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+# Initialize extensions
+db.init_app(app)
+bcrypt = Bcrypt(app)
+CORS(app, supports_credentials=True)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    email = request.json.get("email")
+    password = request.json.get("password")
+
+    if not email or not password:
+        return jsonify({"error": "Email and password are required"}), 400
+
+    user_exists = User.query.filter_by(email=email).first()
+
+    if user_exists:
+        return jsonify({"error": "Email already exists"}), 409
+
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    new_user = User(email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({
+        "id": new_user.id,
+        "email": new_user.email
+    })
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    email = request.json.get("email")
+    password = request.json.get("password")
+
+    if not email or not password:
+        return jsonify({"error": "Email and password are required"}), 400
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({"error": "User not found"}), 404
+
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Incorrect password"}), 401
+
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    })
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
 
-
-# from flask import Flask
-# from models.dbmodels import db
-# from flask_migrate import Migrate
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# db.init_app(app)
-
-# # Import your models to create tables
-# # import models.dbmodels
-
-
-
-# if __name__ == '__main__':
-#     with app.app_context():
-#         db.create_all()
-#     app.run(debug=True)
