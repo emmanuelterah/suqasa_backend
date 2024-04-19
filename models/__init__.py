63e712ffbc8ff36337a44a005650996f7974fd94
Dndinya <derrick.ndinya@gmail.com>
2024-04-19 17:46:43 +0300

    Revert merge

---
+++ models/__init__.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+# from property import Property
+# from landlord import Landlord
+# from tenant import Tenant
+# from lease_agreement import LeaseAgreement
+# from maintenance import MaintenanceRequest
+# from payment import Payment
+from models import db
+
+db = SQLAlchemy()
+
+
+
+
+
