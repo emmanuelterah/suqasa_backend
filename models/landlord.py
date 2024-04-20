59c74874db7fddd797f47513284195a3c4672d9b
Enoch Kibet <enochblake9@gmail.com>
2024-04-20 22:30:33 +0300

    done

--- models/landlord.py
+++ models/landlord.py
@@ -1,20 +1,23 @@
-# # from flask_sqlalchemy import SQLAlchemy
-# # from sqlalchemy_serializer import SerializerMixin
-# # from .dbmodels import db
+# # # from flask_sqlalchemy import SQLAlchemy
+# # # from sqlalchemy_serializer import SerializerMixin
+# # # from .dbmodels import db
 
-# # class Landlord(db.Model, SerializerMixin):
-# #     __tablename__ = 'landlords'
+# # # class Landlord(db.Model, SerializerMixin):
+# # #     __tablename__ = 'landlords'
 
-# #     LandlordID = db.Column(db.Integer, primary_key=True)
-# #     Name = db.Column(db.String)
-# #     ContactInfo = db.Column(db.String)
-# #     BankAcctInfo = db.Column(db.String)
-# #     Address = db.Column(db.String)
+# # #     LandlordID = db.Column(db.Integer, primary_key=True)
+# # #     Name = db.Column(db.String)
+# # #     ContactInfo = db.Column(db.String)
+# # #     BankAcctInfo = db.Column(db.String)
+# # #     Address = db.Column(db.String)
 
-# #     properties = db.relationship("Property", back_populates="landlord")
+# # #     properties = db.relationship("Property", back_populates="landlord")
 
 # from flask_sqlalchemy import SQLAlchemy
 # from sqlalchemy_serializer import SerializerMixin
+# from .dbmodels import db
+# from models.property import Property
+
 
 # db = SQLAlchemy()
 
@@ -27,12 +30,34 @@
 #     BankAcctInfo = db.Column(db.String)
 #     Address = db.Column(db.String)
 
-#     properties = db.relationship("Property", back_populates="landlord")
+#     # Define the relationship with Property using actual column names
+#     properties = db.relationship("Property", backref="landlord", foreign_keys="[Property.LandlordID]", primaryjoin="Landlord.LandlordID == Property.LandlordID")
+
+#     def __repr__(self):
+#         return f'<Landlord {self.Name}>'
+
+
+# from .dbmodels import db
+
+# class Landlord(db.Model):
+#     __tablename__ = 'landlords'
+
+#     LandlordID = db.Column(db.Integer, primary_key=True)
+#     Name = db.Column(db.String)
+#     ContactInfo = db.Column(db.String)
+#     BankAcctInfo = db.Column(db.String)
+#     Address = db.Column(db.String)
+
+#     # Define the relationship with Property using a string-based relationship definition
+#     properties = db.relationship("Property", backref="landlord", foreign_keys="[Property.LandlordID]")
+
+#     def __repr__(self):
+#         return f'<Landlord {self.Name}>'
+
 
 from .dbmodels import db
-from sqlalchemy_serializer import SerializerMixin
 
-class Landlord(db.Model, SerializerMixin):
+class Landlord(db.Model):
     __tablename__ = 'landlords'
 
     LandlordID = db.Column(db.Integer, primary_key=True)
@@ -41,5 +66,9 @@ class Landlord(db.Model, SerializerMixin):
     BankAcctInfo = db.Column(db.String)
     Address = db.Column(db.String)
 
-    properties = db.relationship("Property", back_populates="landlord")
+    # Define the relationship with Property using a string-based relationship definition
+    properties = db.relationship("Property", backref="landlords", foreign_keys="[Property.LandlordID]")
+
+    def __repr__(self):
+        return f'<Landlord {self.Name}>'
 
