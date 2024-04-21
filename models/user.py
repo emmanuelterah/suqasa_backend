5877c1aa393d9c504c499706a34095d5ee494464
Dndinya <derrick.ndinya@gmail.com>
2024-04-21 23:56:09 +0300

    User

--- models/user.py
+++ models/user.py
@@ -1,13 +1,21 @@
-# from flask_sqlalchemy import SQLAlchemy
-# from uuid import uuid4
-# from .dbmodels import db
+from flask_sqlalchemy import SQLAlchemy
+from uuid import uuid4
+from werkzeug.security import generate_password_hash, check_password_hash
+from .dbmodels import db
 
-# def get_uuid():
-#     return uuid4().hex
+def get_uuid():
+    return uuid4().hex
 
-# class User(db.Model):
-#     __tablename__ = 'users'
+class User(db.Model):
+    __tablename__ = 'users'
 
-#     id = db.Column(db.String(11), primary_key=True, unique=True, default=get_uuid)
-#     email = db.Column(db.String(150), unique=True)
-#     password = db.Column(db.Text, nullable=False)
+    id = db.Column(db.String(11), primary_key=True, unique=True, default=get_uuid)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.Text, nullable=False)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
