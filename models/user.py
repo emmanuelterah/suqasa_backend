8e8f38fbe04b2669a75a7b7389b86d9bacc9f51f
Emmanuel Terah <emmanuelterah98@gmail.com>
2024-04-22 16:18:29 +0300

    latest master

--- models/user.py
+++ models/user.py
@@ -9,17 +9,17 @@ def get_uuid():
 class User(db.Model):
     tablename = 'users'
 
-id = db.Column(db.String(11), primary_key=True, unique=True, default=get_uuid)
-username = db.Column(db.String(100), unique=True, nullable=False)
-email = db.Column(db.String(150), unique=True)
-user_type = db.Column(db.String(50))
-password = db.Column(db.Text, nullable=False)
-   
+    id = db.Column(db.String(11), primary_key=True, unique=True, default=get_uuid)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True)
+    user_type = db.Column(db.String(50))
+    password = db.Column(db.Text, nullable=False)
+    
 
-   
+    
 
-def set_password(self, password):
-    self.password_hash = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-def check_password(self, password):
-    return check_password_hash(self.password_hash, password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
