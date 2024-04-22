e97d101b775e8cc28511173fabb41ca4446f6599
Emmanuel Terah <emmanuelterah98@gmail.com>
2024-04-22 15:36:54 +0300

    latest master

--- models/user.py
+++ models/user.py
@@ -9,11 +9,11 @@ def get_uuid():
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.String, primary_key=True, unique=True , default=get_uuid)
+    id = db.Column(db.String, primary_key=True, unique=True, default=get_uuid)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True)
     user_type = db.Column(db.String(50))
-    password = db.Column(db.Text, nullable=False)  # Renamed to password_hash
+    password_hash = db.Column(db.Text, nullable=False)  # Changed column name to password_hash
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
